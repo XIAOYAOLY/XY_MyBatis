d8665d78ca1c410d55e137280ac966a1d899a340
XIAOYAOLY <82129666+XIAOYAOLY@users.noreply.github.com>
2021-04-08 10:43:59 +0800

    Add files via upload

---
+++ design_pattern/src/main/java/com/lagou/simpleFactory/ComputerFactory.java
@@ -0,0 +1,18 @@
+package com.lagou.simpleFactory;
+
+public class ComputerFactory {
+
+    public static Computer createComputer(String type){
+        Computer computer =null;
+        switch (type){
+            case "lenovo":
+                computer = new LenovoComputer();
+                break;
+            case "hp":
+                computer = new HpComputer();
+        }
+
+        return  computer;
+    }
+
+}
